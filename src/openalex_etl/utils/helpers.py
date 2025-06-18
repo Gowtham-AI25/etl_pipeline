b1b5d1f08bfe55238851a7a6e9cba077ef079d46
Gowtham <gowthamgoli02@gmail.com>
2025-06-18 11:56:51 +0530

    version 1 extarct in etl  pipeline is completed

---
+++ src/openalex_etl/utils/helpers.py
@@ -0,0 +1,19 @@
+# Shared utility functions (logging, config loading, etc.)
+import yaml
+
+def config_yml(config_path, parent_key, updates: dict ):
+  with open(config_path , 'r') as f:
+    config = yaml.safe_load(f) or {}
+
+  if parent_key not in config:
+    config[parent_key] = {}
+
+  for key, value in updates.items():
+    config[parent_key][key] = value
+
+  with open(config_path, 'w') as f:
+    yaml.safe_dump(config, f, sort_keys=False)
+
+  print(f"updated {parent_key} in config")
+  return config
+
